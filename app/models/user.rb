e222da7ae562feb5670fe44a2f2a01fa0258ce51
Wang Yeming <wangyeming@dotide.com>
2014-08-05 20:07:07 +0800

    Format modify

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable,
          :validatable
   attr_accessor :login
-  validates :username,:uniqueness => {:case_sensitive => false}
+  validates :username, uniqueness: { case_sensitive: false }
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions)
+      .where(['lower(username) = :value OR lower(email) = :value',
+              { value: login.downcase }]).first
     else
       where(conditions).first
     end
   end
-=begin
-  before_create :create_login
-
-  def create_login
-    email = self.email.split(/@/)
-    login_taken = User.where( :login => email[0]).first
-    unless login_taken
-      self.login = email[0]
-    else
-      self.login = self.email
-    end
-  end
-=end
 end
