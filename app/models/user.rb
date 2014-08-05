3545923bf8e6b1ed561ed28b56ed218e438f6ded
Wang Yeming <wangyeming@dotide.com>
2014-08-05 22:45:10 +0800

    Add email valid for signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :validatable
+         :validatable, :confirmable, :lockable
   attr_accessor :login
   validates :username, uniqueness: { case_sensitive: false }
 
